a86a87403e0f5e85ec2f4017d252d755d09edf7b
deboradaddazio@yahoo.it <deboradaddazio@yahoo.it>
2025-09-03 12:52:59 +0200

    first commit

--- Elf_not_on_the_shelf.py
+++ Elf_not_on_the_shelf.py
@@ -1,1 +1,25 @@
-print("noooo")
+print("...")
+
+""" 
+This looks like something where the program needs to run through the data and ´append´ only the relevant results 
+on a list. 
+First thing, define limits, dvs max number for each colors, so possibility or not can be matched.
+Create a list to store the ID values which are possible.
+Also program needs to understand how to read the data since is not a nice table format (if I had a paid version of
+some data AI tools I´d convert the data to a table to be analyzed by SQL, to skip scripting ´how to read this´ on python, 
+I did try but just took more time).
+Then create a for-loop to go through the data once and save only the possible ID in the declared list.
+By the end, count by hand or easily with sum().
+
+Logic:
+- Define the limit 
+- Tell the program how to read the data I have (;, .)...
+...and handling the data I do not have (like colors missing)
+- Parse all data (once)
+- Append (collect) only the ID which are possible when all the condition on the same line are met
+- I may also make a sum to count all the values collected (ID) in the list, but I guess being a small dataset they can be counted easily
+
+Could I write this -syntax- with my current skills? Nope. 
+But I have nice copilot here and AI. Best I can do without is if/else"""
+
+#import data
